6da4c4497f65de767276cae000f992213d64ab34
crazytuzi <957824770@qq.com>
2019-10-29 23:38:26 +0800

    Setup Hearing  Sense

--- Source/FPSGame/Public/FPSAIGuard.h
+++ Source/FPSGame/Public/FPSAIGuard.h
@@ -19,7 +19,11 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, Category = "Components")
 	class UPawnSensingComponent* PawnSensingComp;
+
+	UFUNCTION()
+	void OnPawnSeen(APawn* SeenPawn);
 
 public:	
 	// Called every frame
